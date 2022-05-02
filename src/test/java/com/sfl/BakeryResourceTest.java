c90c2ae76ef1816dda605b9945f22a711a354dc7
Stéphane <sfleury72@gmail.com>
2022-05-02 14:23:11 +0200

    Initial commit

---
+++ src/test/java/com/sfl/BakeryResourceTest.java
@@ -0,0 +1,21 @@
+package com.sfl;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class BakeryResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/hello")
+          .then()
+             .statusCode(200)
+             .body(is("Hello RESTEasy"));
+    }
+
+}
---
+++ src/test/java/com/sfl/NativeBakeryResourceIT.java
@@ -0,0 +1,9 @@
+package com.sfl;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeBakeryResourceIT extends BakeryResourceTest {
+
+    // Execute the same tests but in native mode.
+}
